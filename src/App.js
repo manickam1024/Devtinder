46ac200ee3c8dd9fb78cbc02ab96da3475b1b8d5
manickam1024 <manicyam1234@gmail.com>
2025-09-11 18:06:15 +0530

    middleware-chaining

--- src/App.js
+++ src/App.js
@@ -7,14 +7,24 @@ app.use(express.json());
 
 app.use(
   "/login/profile",
-  // (req, res, next) => {
-  //   // res.send("profile page");
-  //   next();
-  // },
-  // (req, res) => {
-  //   res.send("profile page ");
-  // },
+  (req, res, next) => {
+    // res.send("profile page");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("profile page 2");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("profile page 3");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("profile page 4");
+    next();
+  },
 );
+
 app.use("/login", (req, res) => {
   res.send(" the login page");
 });
@@ -24,7 +34,8 @@ app.post("/sendform", (req, res) => {
   res.end("promise resolved");
 });
 app.use("/", (req, res, next) => {
-  res.send(" home page ");
+  // res.send(" home page ");
+  next();
 });
 app.listen(4444, () => {
   console.log("server listening on 4444");
