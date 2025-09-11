5b845c00c2bd400590c114c4f519d677b72af564
manickam1024 <manicyam1234@gmail.com>
2025-09-11 16:03:13 +0530

    request ,route,middlwares

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.text());
+app.use(express.json());
+
+app.use(
+  "/login/profile",
+  // (req, res, next) => {
+  //   // res.send("profile page");
+  //   next();
+  // },
+  // (req, res) => {
+  //   res.send("profile page ");
+  // },
+);
+app.use("/login", (req, res) => {
+  res.send(" the login page");
+});
+
+app.post("/sendform", (req, res) => {
+  res.send("this is the body u sent " + req.body);
+  res.end("promise resolved");
+});
 app.use("/", (req, res, next) => {
-  next();
+  res.send(" home page ");
 });
-
-app.get("/login", (req, res) => {
-  res.send("this is the login page request");
-});
-
 app.listen(4444, () => {
   console.log("server listening on 4444");
 });
